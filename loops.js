e8d16a1dc9bfcddf59aacc107636ec0799f830f4
Philip Roth <phil.roth077@gmail.com>
2019-05-31 00:02:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,8 @@
 
-function forLoop(array) {
-  for (var i = 0; i < 25; i++) {
-    array[i]
-  }
-  return array
-}
+
 
 function whileLoop(n) {
-  while (n === 0) {
-    n--
+  while (n <= 0) {
+    return n--
   }
-
 }
